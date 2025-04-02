85c6fc7d935df6f7debfee6265b88c79d3786c4c
Petar Rakocevic <petar.r.rakocevic@gmail.com>
2025-04-02 12:48:42 +0200

    Connected backend and frontend for notes

--- part3/notes/backend/index.js
+++ part3/notes/backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const requestLogger = (request, response, next) => {
@@ -12,6 +13,8 @@ const requestLogger = (request, response, next) => {
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -89,7 +92,7 @@ app.delete("/api/notes/:id", (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
