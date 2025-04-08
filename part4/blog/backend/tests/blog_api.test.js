46229c000e7c486af73f58b1187ff3e88093912b
PetarR21 <petar.r.rakocevic@gmail.com>
2025-04-08 16:24:39 +0200

    Finished exercise 4.10

--- part4/blog/backend/tests/blog_api.test.js
+++ part4/blog/backend/tests/blog_api.test.js
@@ -7,6 +7,7 @@ const app = require('../app')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
+const { forEach } = require('lodash')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
@@ -35,6 +36,44 @@ test('a specific blog is within returned blogs', async () => {
   const titles = response.body.map((b) => b.title)
 
   assert(titles.includes(helper.initialBlogs[0].title))
+})
+
+test('unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+  const blogs = response.body
+
+  blogs.forEach((blog) => {
+    assert(blog.id)
+  })
+})
+
+test('a valid blog can be added', async () => {
+  const newBlogObject = {
+    title: 'TestTitle',
+    author: 'TestAuthor',
+    likes: 0,
+    url: 'TestUrl',
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlogObject)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const blogObjects = response.body.map((blog) => {
+    return {
+      title: blog.title,
+      author: blog.author,
+      likes: blog.likes,
+      url: blog.url,
+    }
+  })
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+  assert(blogObjects.find((blog) => JSON.stringify(blog) === JSON.stringify(newBlogObject)))
 })
 
 after(async () => {
