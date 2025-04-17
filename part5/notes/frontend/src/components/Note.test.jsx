eb253b78567fb798c9d174240a4006f08d5d4178
PetarR21 <petar.r.rakocevic@gmail.com>
2025-04-17 14:41:04 +0200

    Working on part 5c

--- part5/notes/frontend/src/components/Note.test.jsx
+++ part5/notes/frontend/src/components/Note.test.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Note from './Note'
 
 test('renders component', () => {
@@ -10,8 +11,26 @@ test('renders component', () => {
 
   render(<Note note={note} />)
 
-  const element = screen.getByText(
-    'Component testing is done with react-testing-library'
-  )
+  const element = screen.getByText('Component testing is done with react-testing-library')
+
+  screen.debug(element)
+
   expect(element).toBeDefined()
+})
+
+test('clicking the button calls event handler once', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true,
+  }
+
+  const mockHandler = vi.fn()
+
+  render(<Note note={note} toggleImportance={mockHandler} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('make note important')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
