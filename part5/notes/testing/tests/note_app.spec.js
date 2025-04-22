ca48025fda49448f62832dccf2e2a34f89b2df69
PetarR21 <petar.r.rakocevic@gmail.com>
2025-04-22 15:33:24 +0200

    Working on part 5d

--- part5/notes/testing/tests/note_app.spec.js
+++ part5/notes/testing/tests/note_app.spec.js
@@ -1,11 +1,24 @@
-const { test, expect } = require('@playwright/test')
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
-test('front page can be opened', async ({ page }) => {
-  await page.goto('http://localhost:5173')
+describe('Note app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
 
-  const locator = await page.getByText('Notes')
-  await expect(locator).toBeVisible()
-  await expect(
-    page.getByText('Note app, Department of Computer Science, University of Helsinki 2025')
-  ).toBeVisible()
+  test('front page can be opened', async ({ page }) => {
+    const locator = await page.getByText('Notes')
+    await expect(locator).toBeVisible()
+    await expect(
+      page.getByText('Note app, Department of Computer Science, University of Helsinki 2025')
+    ).toBeVisible()
+  })
+
+  test('login form can be opened', async ({ page }) => {
+    await page.getByRole('button', { name: 'login' }).click()
+    await page.getByTestId('username').fill('mluukkai')
+    await page.getByTestId('password').fill('salainen')
+    await page.getByRole('button', { name: 'login' }).click()
+
+    await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+  })
 })
