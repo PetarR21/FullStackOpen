6cc95b5a2949387775c726eec40c367b184999bf
PetarR21 <petar.r.rakocevic@gmail.com>
2025-05-28 21:08:17 +0200

    Working on part 9a

---
+++ part9/fs-open/multiplier.ts
@@ -0,0 +1,27 @@
+interface MultiplyValues {
+  value1: number;
+  value2: number;
+}
+
+const parseArguments = (args: string[]): MultiplyValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3]),
+    };
+  } else {
+    throw new Error('Provided values were not numbers');
+  }
+};
+
+const multiplicator = (a: number, b: number, printText: string) => {
+  console.log(printText, a * b);
+};
+
+const a: number = Number(process.argv[2]);
+const b: number = Number(process.argv[3]);
+
+multiplicator(a, b, `Multiplied numbers ${a} and ${b}, the result is:`);
