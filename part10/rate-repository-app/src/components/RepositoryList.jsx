c3c5229115652b28f2978593a588a3271111cfc7
Petar Rakocevic <petar.r.rakocevic@gmail.com>
2025-07-01 16:31:27 +0200

    Finished exercise 10.23

--- part10/rate-repository-app/src/components/RepositoryList.jsx
+++ part10/rate-repository-app/src/components/RepositoryList.jsx
@@ -1,9 +1,11 @@
 import useRepositories from '../hooks/useRepositories'
 import { FlatList, View, StyleSheet, Dimensions, Pressable } from 'react-native'
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context'
-
+import { Picker } from '@react-native-picker/picker'
 import RepositoryItem from './RepositoryItem'
 import { useNavigate } from 'react-router-native'
+import { useState, useRef } from 'react'
+import theme from '../theme'
 
 const styles = StyleSheet.create({
   separator: {
@@ -16,10 +18,25 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />
 
-export const RepositoryListContainer = ({ repositories, navigateTo }) => {
+export const RepositoryListContainer = ({
+  repositories,
+  navigateTo,
+  selectedValue,
+  setSelectedValue,
+}) => {
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
     : []
+
+  const pickerRef = useRef()
+
+  function open() {
+    pickerRef.current.focus()
+  }
+
+  function close() {
+    pickerRef.current.blur()
+  }
 
   return (
     <SafeAreaProvider>
@@ -41,6 +58,26 @@ export const RepositoryListContainer = ({ repositories, navigateTo }) => {
               <RepositoryItem item={item} />
             </Pressable>
           )}
+          ListHeaderComponent={
+            <Picker
+              style={{ color: theme.colors.textPrimary }}
+              ref={pickerRef}
+              selectedValue={selectedValue}
+              onValueChange={(itemValue, itemIndex) =>
+                setSelectedValue(itemValue)
+              }
+            >
+              <Picker.Item
+                label='Select an item...'
+                value=''
+                enabled={false}
+                style={{ color: theme.colors.textSecondary }}
+              />
+              <Picker.Item label='Latest repositories' value='latest' />
+              <Picker.Item label='Highest rated repositories' value='highest' />
+              <Picker.Item label='Lowest rated repositories' value='lowest' />
+            </Picker>
+          }
         />
       </SafeAreaView>
     </SafeAreaProvider>
@@ -48,7 +85,9 @@ export const RepositoryListContainer = ({ repositories, navigateTo }) => {
 }
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories()
+  const [selectedValue, setSelectedValue] = useState('')
+  const { repositories } = useRepositories(selectedValue)
+
   const navigate = useNavigate()
 
   const navigateTo = (id) => {
@@ -59,6 +98,8 @@ const RepositoryList = () => {
     <RepositoryListContainer
       repositories={repositories}
       navigateTo={navigateTo}
+      selectedValue={selectedValue}
+      setSelectedValue={setSelectedValue}
     />
   )
 }
--- part10/rate-repository-app/src/hooks/useRepositories.js
+++ part10/rate-repository-app/src/hooks/useRepositories.js
@@ -1,10 +1,22 @@
 import { GET_REPOSITORIES } from '../graphql/queries'
 import { useQuery } from '@apollo/client'
 
-const useRepositories = () => {
+const useRepositories = (selectedValue) => {
   // eslint-disable-next-line no-unused-vars
+  console.log(selectedValue)
+
+  let variables = {}
+  if (selectedValue === 'latest') {
+    variables = { orderBy: 'CREATED_AT', orderDirection: 'DESC' }
+  } else if (selectedValue === 'highest') {
+    variables = { orderBy: 'RATING_AVERAGE', orderDirection: 'DESC' }
+  } else if (selectedValue === 'lowest') {
+    variables = { orderBy: 'RATING_AVERAGE', orderDirection: 'ASC' }
+  }
+
   const { data, loading } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network',
+    variables: variables,
   })
 
   return { repositories: data ? data.repositories : undefined }
