aef9a830c306a4eac1c392fa0e32cf1a71d12940
Petar Rakocevic <petar.r.rakocevic@gmail.com>
2025-07-21 18:57:45 +0200

    Working on Development in containers

--- part12/part12-containers-applications/hello-front/src/App.jsx
+++ part12/part12-containers-applications/hello-front/src/App.jsx
@@ -9,15 +9,17 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href='https://vite.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1>TESING !!!VOLUME MOUNT</h1>
+      <h2>Volume Mount Works!</h2>
+      <h3>Hello</h3>
+      <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,7 +27,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
     </>
